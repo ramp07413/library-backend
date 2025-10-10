befaebfaedc24cd1fe145a80c9ff252e4e070e2b
ramp07412 <ramp07413@gmail.com>
2025-10-10 16:24:52 +0530

    added libries in compnay

--- controllers/company/libraryController.js
+++ controllers/company/libraryController.js
@@ -41,6 +41,47 @@ export const registerLibrary = async(req, res, next)=>{
 }
 
 
+export const updateLibrary = async(req, res, next)=>{
+    try {
+
+        const libraryId  = req.params.id
+
+        const {libraryName, libraryEmail, libraryContact, address_line1,address_line2,city,state,pincode,subscription_plan,subscription_status} = req.body || {}
+        
+        if (!errors.isEmpty()) {
+            return next({ status: 400, errors: errors.array() });
+        }
+
+        let librarydata = await libraries.findOne({libraryEmail})
+
+        if(librarydata){
+            return res.status(400).json({
+                success : false,
+                message : "library already registered !"
+            })
+        }
+
+        librarydata = await libraries.create({
+            libraryName, libraryEmail, libraryContact, address_line1,address_line2,city,state,pincode,subscription_plan,subscription_status
+        })
+
+         res.status(200).json({
+                success : true,
+                message : "library register successfully !",
+                librarydata
+            })
+        
+    } catch (err) {
+        console.error(err)
+        res.status(500).josn({
+            success : false,
+            message : err.message
+        })
+    }
+}
+
+
+
 
 export const getLibrares = async(req, res, next)=>{
     try {
